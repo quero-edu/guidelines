e3403abc62511917dd62deedf51bab15af8c18f1
vitebo <andre.vitebo@redealumni.com>
2020-12-05 23:32:24 -0300

    :wrench: (stylelint) add new rules
    extends style-lint-standard
    add style-lint-order rules
    
    Signed-off-by: vitebo <andre.vitebo@redealumni.com>

--- packages/stylelint-config-base/stylelint.config.js
+++ packages/stylelint-config-base/stylelint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-order'],
   rules: {
-    'block-no-empty': true,
     'color-named': 'never',
+    'declaration-colon-newline-after': 'always-multi-line',
     'declaration-no-important': true,
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-alphabetical-order': true,
   },
 };
