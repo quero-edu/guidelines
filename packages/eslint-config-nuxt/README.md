67bd9c66d6a58a9cfc765b9b149ad2994dda848a
Vinícius Hoyer <contact@vhoyer.dev>
2024-02-07 23:40:23 -0300

    feat(nuxt): create eslint-config-nuxt

---
+++ packages/eslint-config-nuxt/README.md
@@ -0,0 +1,15 @@
+# `@quero/eslint-config-nuxt`
+
+You only need to install `elsint` and this package;
+
+```base
+pnpm add -D eslint @quero/elsint-config-nuxt
+# or
+npm install --save-dev eslint @quero/elsint-config-nuxt
+```
+
+```javascript
+module.exports = {
+  extends: ['@quero/eslint-config-nuxt'],
+}
+```
---
+++ packages/eslint-config-nuxt/index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.mjs', '*.cjs'],
+      extends: ['@quero/eslint-config-base'],
+    },
+    {
+      files: ['*.ts'],
+      extends: ['@quero/eslint-config-typescript'],
+    },
+    {
+      files: ['*.vue'],
+      extends: [
+        '@quero/eslint-config-typescript',
+        '@quero/eslint-config-vue',
+      ],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+      },
+      rules: {
+        'vue/component-name-in-template-casing': [
+          'error',
+          'PascalCase',
+          {
+            registeredComponentsOnly: true,
+            ignores: [],
+          },
+        ],
+      },
+    },
+    {
+      // These pages are not used directly by users so they can have one-word names.
+      files: [
+        '**/pages/**/*.{ts,vue}',
+        '**/layouts/**/*.{ts,vue}',
+        '**/app.{ts,vue}',
+        '**/error.{ts,vue}',
+      ],
+      rules: { 'vue/multi-word-component-names': 'off' },
+    },
+    {
+      // Pages and layouts are required to have a single root element if transitions are enabled.
+      files: ['**/pages/**/*', '**/layouts/**/*'],
+      rules: { 'vue/no-multiple-template-root': 'error' },
+    },
+  ],
+};
